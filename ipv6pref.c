4f2282e87bbd191063fb656761f606a2897a04be
Stefan Tomanek <stefan.tomanek@wertarbyte.de>
2015-11-26 21:09:02 +0100

    refactor preference selection code

--- ipv6pref.c
+++ ipv6pref.c
@@ -12,26 +12,34 @@
 #define __USE_GNU
 #include <dlfcn.h>
 
+static int get_addr_preference(void) {
+	char *pref = getenv("IPV6PREF_ADDR");
+	int v = 0;
+	if (!pref) {
+		v = -1;
+	} else
+	if (strcasecmp(pref, "pub") == 0 ||
+	    strcasecmp(pref, "public") == 0) {
+		v = IPV6_PREFER_SRC_PUBLIC;
+	} else
+	if (strcasecmp(pref, "temp") == 0 ||
+	    strcasecmp(pref, "tmp") == 0) {
+		v = IPV6_PREFER_SRC_TMP;
+	}
+	return v;
+}
+
 int socket(int domain, int type, int protocol) {
 	static int (*socket_real)(int, int, int) = NULL;
 	if (!socket_real) socket_real = dlsym(RTLD_NEXT, "socket");
 
 	int fd = socket_real(domain, type, protocol);
 
-	char *pref = getenv("IPV6PREF_ADDR");
-	if (fd >= 0 && domain == AF_INET6 && pref) {
-		int value = 0;
-		if (strcasecmp(pref, "pub") == 0 ||
-		    strcasecmp(pref, "public") == 0) {
-			value = IPV6_PREFER_SRC_PUBLIC;
-		} else
-		if (strcasecmp(pref, "temp") == 0 ||
-		    strcasecmp(pref, "tmp") == 0) {
-			value = IPV6_PREFER_SRC_TMP;
-		}
-		if (value) {
+	if (fd >= 0 && domain == AF_INET6) {
+		int pref = get_addr_preference();
+		if (pref > 0) {
 			setsockopt(fd, IPPROTO_IPV6, IPV6_ADDR_PREFERENCES,
-			           &value, sizeof(value));
+			           &pref, sizeof(pref));
 		}
 	}
 	return fd;
