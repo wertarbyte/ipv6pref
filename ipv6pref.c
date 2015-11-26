3af9e1af19947ae97a249b854144b29edd3f9831
Stefan Tomanek <stefan.tomanek@wertarbyte.de>
2015-11-26 18:54:57 +0100

    use environment variable to switch to public address

--- ipv6pref.c
+++ ipv6pref.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <strings.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <linux/ipv6.h>
@@ -16,9 +17,22 @@ int socket(int domain, int type, int protocol) {
 	if (!socket_real) socket_real = dlsym(RTLD_NEXT, "socket");
 
 	int fd = socket_real(domain, type, protocol);
-	if (fd >= 0 && domain == AF_INET6) {
-		int value = IPV6_PREFER_SRC_PUBLIC;
-		setsockopt(fd, IPPROTO_IPV6, IPV6_ADDR_PREFERENCES, &value, sizeof(value));
+
+	char *pref = getenv("IPV6PREF_ADDR");
+	if (fd >= 0 && domain == AF_INET6 && pref) {
+		int value = 0;
+		if (strcasecmp(pref, "pub") == 0 ||
+		    strcasecmp(pref, "public") == 0) {
+			value = IPV6_PREFER_SRC_PUBLIC;
+		} else
+		if (strcasecmp(pref, "temp") == 0 ||
+		    strcasecmp(pref, "tmp") == 0) {
+			value = IPV6_PREFER_SRC_TMP;
+		}
+		if (value) {
+			setsockopt(fd, IPPROTO_IPV6, IPV6_ADDR_PREFERENCES,
+			           &value, sizeof(value));
+		}
 	}
 	return fd;
 }
