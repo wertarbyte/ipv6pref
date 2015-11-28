7f3646fa2bf04f189dba337412e28cf38f12b0c7
Stefan Tomanek <stefan.tomanek@wertarbyte.de>
2015-11-28 10:19:09 +0100

    do not modify LD_PRELOAD if not needed

--- scripts/ipv6pref.sh
+++ scripts/ipv6pref.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 LIB=/usr/local/lib/ipv6pref.so
 
-if [ -e "$LIB" ]; then
-	export LD_PRELOAD="$LIB"
-else
-	echo "Unable to preload library '$LIB'" >&2
-fi
-
 dbg() {
 	[ "${IPV6PREF_DEBUG:-}" ] || return
 	echo "ipv6pref:" "$@" >&2
 }
 
 if ! [ "${IPV6PREF_WAS_HERE:-}" ]; then
+	if [ -e "$LIB" ]; then
+		export LD_PRELOAD="$LIB"
+	else
+		echo "Unable to preload library '$LIB'" >&2
+	fi
+
 	WRAPPER="$(basename $0)"
 	PREF=""
 	case "$WRAPPER" in
